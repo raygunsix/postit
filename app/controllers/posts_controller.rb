7863bb4726e539e10179d19471472cbd471777c2
Chris Reid <chris@raygun6.com>
2014-09-04 17:04:05 -0700

    adds initial post controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,46 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:post] = "You post was saved."
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update
+      flash[:post] = "You post was updated."
+      redirect_to @post
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
 end
