5c945d83c45b90caf9814ffc282331e72b68bb68
Chris Reid <chris@raygun6.com>
2014-09-08 12:14:14 -0700

    adds initial categories controller and forms

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,47 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update]
+
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(post_params)
+
+    if @category.save
+      flash[:category] = "You category was saved."
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(post_params)
+      flash[:category] = "Your category was updated."
+      redirect_to categories_path
+    else
+      render :edit
+    end
+
+  end
+
+  private
+
+  def post_params
+    params.require(:category).permit(:name)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+end
