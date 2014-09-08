c0143927c1dd9d874f04c30e9ff6d0227cffb8da
Chris Reid <chris@raygun6.com>
2014-09-08 12:27:39 -0700

    adds categories show controller action and view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,12 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:edit, :update]
+  before_action :set_category, only: [:show, :edit, :update]
 
 
   def index
     @categories = Category.all
+  end
+
+  def show
   end
 
   def new
