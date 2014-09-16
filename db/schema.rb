56905937fda552deb30fbc01554f7935a983261d
Chris Reid <chris@raygun6.com>
2014-09-16 16:37:37 -0700

    adds password_digest column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827194529) do
+ActiveRecord::Schema.define(version: 20140916233524) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140827194529) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
