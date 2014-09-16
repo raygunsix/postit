3a2169b94c39cbe53262ccedcbd302514985ab70
Chris Reid <chris@raygun6.com>
2014-09-16 16:06:08 -0700

    adds display_datetime helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def fix_url(str)
     str.start_with?("http://") ? str : "http://#{str}"
   end
+
+  def display_datetime(dt)
+    dt.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
