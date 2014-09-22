e8e56de211c24928e976bc4cfaf84f75237391f0
Chris Reid <chris@raygun6.com>
2014-09-22 12:58:25 -0700

    requires usnernames to be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
 
   has_secure_password validations: false
 
-  validates :username, presence: true, length: { minimum: 3 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 3 }
 end
