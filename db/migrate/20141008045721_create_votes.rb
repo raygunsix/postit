e96d7bf1785aaefeba0ae90a15f683271ba4e966
Chris Reid <chris@raygun6.com>
2014-10-08 10:13:28 -0700

    uses shortcut syntax for creating polymorphic columns

--- db/migrate/20141008045721_create_votes.rb
+++ db/migrate/20141008045721_create_votes.rb
@@ -3,8 +3,7 @@ class CreateVotes < ActiveRecord::Migration
     create_table :votes do |t|
       t.boolean :vote
       t.integer :user_id
-      t.string :voteable_type
-      t.integer :voteable_id
+      t.references :voteable, polymorphic: true
     end
   end
 end
