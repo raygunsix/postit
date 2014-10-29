8fd07a378e4ea10d873879b0002b51c9b3156480
Chris Reid <chris@raygun6.com>
2014-10-28 22:44:55 -0700

    adds slugs to users and categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :password, presence: true, length: { minimum: 3 }, on: :create
+
+  before_save :generate_slug
+
+  def generate_slug
+    self.slug = self.username.parameterize
+  end
+
+  def to_param
+    self.slug
+  end
 end
