3faaa1acfd61d3b9fa7f57469080fdffa4354468
Chris Reid <chris@raygun6.com>
2014-10-31 16:28:58 -0700

    extracts common votes code to module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,14 @@
 class Post < ActiveRecord::Base
+  include Voteable
+
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true
 
   before_save :generate_slug
-
-  def total_votes
-    up_votes - down_votes
-  end
 
   def generate_slug
     self.slug = self.title.parameterize
@@ -39,16 +36,6 @@ class Post < ActiveRecord::Base
 
   def to_param
     self.slug
-  end
-
-  private
-
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-
-  def down_votes
-    self.votes.where(vote: false).size
   end
 
 end
