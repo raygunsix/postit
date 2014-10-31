2d3fcc8a0533c07b44f20eeb6e2bd793d671298d
Chris Reid <chris@raygun6.com>
2014-10-31 12:59:06 -0700

    ensures slugs are unique

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,26 @@ class Post < ActiveRecord::Base
 
   def generate_slug
     self.slug = self.title.parameterize
+    make_unique_slug while slug_exists?
+  end
+
+  def make_unique_slug
+    if /-[0-9]+$/.match(self.slug)
+      slug_parts = self.slug.split('-')
+      num = slug_parts[-1].to_i
+      if num > 0
+        num = num + 1
+        slug_parts[-1] = num.to_s
+        self.slug = slug_parts.join("-")
+      end
+    else
+      self.slug << "-2"
+    end
+  end
+
+  def slug_exists?
+    slug = Post.find_by slug: self.slug
+    !!slug
   end
 
   def to_param
